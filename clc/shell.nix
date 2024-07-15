b0f4ea6a7993c1c63765619b7ffa04311886c9e0
Alexander <Desench@proton.me>
2024-07-15 14:39:10 +0300

    update with autobuild using nix-shell

--- clc/shell.nix
+++ clc/shell.nix
@@ -7,6 +7,7 @@ pkgs.mkShell {
 ];
 
 shellHook = ''
+  cargo build --release
   cat << 'EOF' > ~/command_wrapper.sh
 #!/usr/bin/env bash
 while true; do
@@ -14,7 +15,7 @@ while true; do
   if [ "$cmd" = "exit" ]; then
     break
   fi
-  nu -c "/home/gidrex/github/calc.rs/clc/target/release/clc \"$cmd\""
+  nu -c "./target/release/clc \"$cmd\""
 done
 EOF
   chmod +x ~/command_wrapper.sh
