be0d451de013190517dae8bc3b66cd5db4aa38a3
Alexander <Desench@proton.me>
2024-07-15 14:32:04 +0300

    add interesting thing

--- clc/shell.nix
+++ clc/shell.nix
@@ -4,9 +4,21 @@ pkgs.mkShell {
   buildInputs = with pkgs; [
     rustup cargo
     nushell
-  ];
+];
 
-  shellHook = ''
-    exec nu
+shellHook = ''
+  cat << 'EOF' > ~/command_wrapper.sh
+#!/usr/bin/env bash
+while true; do
+  read -p "> " cmd
+  if [ "$cmd" = "exit" ]; then
+    break
+  fi
+  nu -c "/home/gidrex/github/calc.rs/clc/target/release/clc \"$cmd\""
+done
+EOF
+  chmod +x ~/command_wrapper.sh
+  ~/command_wrapper.sh
   '';
 }
+
